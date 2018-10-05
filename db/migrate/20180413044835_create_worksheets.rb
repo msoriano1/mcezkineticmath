00d1a90c44928a1e40147da97aec797dedea1138
cdyap <cdyap@outlook.com>
2018-10-05 19:33:09 +0800

    Fixed migration for Heroku:

--- db/migrate/20180413044835_create_worksheets.rb
+++ db/migrate/20180413044835_create_worksheets.rb
@@ -3,13 +3,18 @@ class CreateWorksheets < ActiveRecord::Migration
     create_table :worksheets do |t|
       t.string :title
       t.string :directions
-      t.references :topic, foreign_key: true
-      t.references :yearlevel, foreign_key: true
-      t.references :admin, foreign_key: true
-      t.references :teacher, foreign_key: true
+      t.references :topic, index: true
+      t.references :yearlevel, index: true
+      t.references :admin, index: true
+      t.references :teacher, index: true
       
       t.timestamps null: false
     end
+
+    add_foreign_key :worksheets, :topics, column: :topic_id 
+    add_foreign_key :worksheets, :yearlevel, column: :yearlevel_id 
+    add_foreign_key :worksheets, :admin, column: :admin_id 
+    add_foreign_key :worksheets, :teacher, column: :teacher_id 
     
   end
 end
